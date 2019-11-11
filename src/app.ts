56e5173f26315573ef75ff8a9a96dfd1eb7264ab
Anubhav Saxena <tuthubpro@gmail.com>
2019-11-12 00:57:22 +0530

    routing of home page

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import express from "express";
 class App {
     public express: any;
+    private router = express.Router();
     constructor() {
         this.express = express();
-        this.mountRoutes();
+        this.routeInitilizer();
     }
-
-    private mountRoutes(): void {
-        const router = express.Router();
-        router.get("/", (req: any, res: any) => {
-      res.send("Hello World!");
-    });
-
-        this.express.use("/", router);
-  }
+    private routing(): void {
+    this.router.get("/", (req: any, res: any) => {
+          res.send("Hello World!");
+        });
+    }
+    private routeInitilizer(): void {
+        this.routing();
+        this.express.use("/", this.router);
+      }
     }
 
 export default new App().express;
